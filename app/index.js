2ca83e06e1988ca90bed5163a43d526bb37185ab
Nick Groos <ntgroos@gmail.com>
2017-02-24 18:42:38 -0600

    started to build scale picker

--- app/index.js
+++ app/index.js
@@ -1,20 +1,31 @@
-var _teoriaPlay = require('../teoria-play');
+var $ = require('jquery');
+var _diatonicator = require('../diatonicator');
+var _scales = require('../lib/scale');
+var scales = _scales.KNOWN_SCALES;
+var $wrapper = $('.wrapper');
 
 function component () {
   var element = document.createElement('div');
 
-  var diatonicator = new _teoriaPlay('a4', 'mixolydian');
-  // var root = diatonicator.chordAt(1);
-  // var second = diatonicator.chordAt(2);
-  // var fourth = diatonicator.chordAt(4);
-  //var sixth = diatonicator.chordAt(6);
+  var diatonicator = new _diatonicator('a4', 'mixolydian');
 
   for (i = 1; i < 8; i++){
     console.log(diatonicator.chordAt(i));
   }
 
-  debugger;
   return element
 }
 
+var buildScalesPicker = function(){
+  var dropdownWrapper = $('<div />', {'class' : 'scales-picker-wrapper', 'css' : {'width' : '200px', 'border-style' : 'solid'}})
+
+  scales.forEach(function(scale){
+    var scaleItem = $('<span />', {'class' : 'scale-item', 'text' : scale.name, 'css' : {}});
+    scaleItem.appendTo(dropdownWrapper);
+  }, this);
+
+  dropdownWrapper.appendTo($wrapper);
+};
+
 document.body.appendChild(component());
+buildScalesPicker();
---
+++ diatonicator.js
@@ -0,0 +1,78 @@
+
+var teoria = require('./teoria')
+
+function Diatonic(root, scale){
+	this.root = teoria.note(root);
+	this.scale = this.root.scale(scale);
+};
+
+Diatonic.prototype = {
+	chordAt: function(index){
+		return this.diatonicChord(this.scale, index - 1);
+	},
+	diatonicChord: function (scale, rootIndex){
+		//debugger;
+		var majorThird = this.majorOrPerfectInterval(scale, rootIndex, 'third');
+		var perfectFifth = this.majorOrPerfectInterval(scale, rootIndex, 'fifth');
+		var majorSeventh = this.majorOrPerfectInterval(scale, rootIndex, 'seventh');
+		
+		if (majorThird && majorSeventh && perfectFifth){
+			// M7
+			return scale.notes()[rootIndex].chord('M7'); // something like this
+		} else if (!majorThird && !majorSeventh && perfectFifth) {
+			// m7
+			return scale.notes()[rootIndex].chord('m7');
+		} else if (majorThird && !majorSeventh && perfectFifth) {
+			// 7
+			return scale.notes()[rootIndex].chord('7');
+		} else if (!majorThird && !majorSeventh && !perfectFifth){
+			return scale.notes()[rootIndex].chord('m7b5');
+		}
+	},
+	getIntervalNotes: function (scale, rootIndex, interval){
+		//debugger;
+
+		switch (interval){
+			case 'third':
+				interval = 2;
+				break;
+			case 'fifth':
+				interval = 4;
+				break;
+			case 'seventh':
+				interval = 6;
+				break;
+			default:
+				return "invalid interval name: use 'third', 'fifth', or 'seventh'"
+		}
+
+		var rootNote = scale.notes()[this.modInterval(rootIndex)];
+
+		// Get the interval note, but check if we need to bump it up an octave
+		var modInt = this.modInterval(rootIndex + interval);
+		var intervalNote = scale.notes()[this.modInterval(modInt)];
+		var intervalNote = modInt < rootIndex ? intervalNote.interval('P8') : intervalNote;
+		
+		return {
+			root : rootNote,
+			interval : intervalNote
+		};
+	},
+	majorOrPerfectInterval: function (scale, rootIndex, intervalName){
+		//debugger;
+		var notes = this.getIntervalNotes(scale, rootIndex, intervalName)
+		
+		if (notes && notes.root && notes.interval){
+			var interval = teoria.interval(notes.root, notes.interval).toString();
+			
+			if (interval){
+				return interval.indexOf('M') >= 0 || interval.indexOf('P') >= 0;
+			}
+		}
+	},
+	modInterval: function(interval){
+		return interval % 7;
+	}
+};
+
+module.exports = Diatonic;
