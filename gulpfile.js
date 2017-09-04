806788619a8435e23859250ffb3a36fe74392a7c
groos <ntgroos@gmail.com>
2017-09-04 14:14:45 -0500

    added browsersync and reworked gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,42 @@
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     webpack = require('webpack-stream');
+    sass = require('gulp-sass');
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  return gulp.src('app/index.js')
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('dist/'));
+gulp.task('default', ['build-js'], function() {
+    browserSync.init({
+        server: {
+            baseDir: './app',
+            routes : {
+                '/node_modules' : './node_modules',
+                '/dist' : './dist'
+            }
+        }
+    })
+
+    gulp.watch('app/*.scss', ['sass']);
+    gulp.watch('app/*.js', ['js-watch']);
+    gulp.watch('app/*.html').on('change', browserSync.reload);
 });
-
-gulp.watch('app/*.js', ['test', 'build-js']);
 
 gulp.task('build-js', function() {
     return gulp.src('app/index.js')
     .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('app/dist'));
 });
 
-gulp.task('test', function() {
-    return gutil.log('saw a change!');
+gulp.task('js-watch', ['build-js'], function(done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
 });
